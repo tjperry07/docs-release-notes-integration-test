0e4493d319a7b6543768ac46f5835c95d796d5d1
Tatiana <tatiana.perry@coalese.io>
2024-03-07 14:20:02 -0500

    fixed release notes not loading, hopefully

--- src/pages/releasenotes.js
+++ src/pages/releasenotes.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '@theme/Layout';
 
 export default function Hello() {
+  useEffect(() => {
+    noticeable.render('widget', 'QEaR7QAA7BkjnKUf05xu');
+    
+    }, []); // The empty array means this effect runs once after the initial render
+
   return (
     <Layout title="Hello" description="Hello React Page">
-        <div id="noticeable-newspage-embed"></div>
+      <div id="noticeable-newspage-embed"></div>
     </Layout>
-  );
+    );
 }
