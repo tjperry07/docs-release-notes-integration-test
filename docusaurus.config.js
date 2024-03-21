9782a1480303362636ea8c36717b49ddc6892648
Tatiana <tatiana.perry@coalese.io>
2024-03-21 15:58:38 -0400

    first commit;

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Coalesce', // Usually your GitHub org/user name.
-  projectName: 'Coalesce Product Documentation', // Usually your repo name.
+  projectName: 'Coalesce Algolia Test', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
